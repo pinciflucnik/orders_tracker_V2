442f77406a8f30b1bc189aad06f0c2ba782cc67a
pinciflucnik <pinciflucnik1@gmail.com>
2025-04-17 17:55:31 +0300

    test

--- src/hooks/useIsLate.js
+++ src/hooks/useIsLate.js
@@ -12,25 +12,19 @@ export default function useIsLate(order){
         setWarning(false);
         const now = new Date()
         const today = moment(now);
-        const formatHelper = order.expected.match(/[0-9]{2}.[0-9]{2}.[0-9]{4}/)[0]
-        const expected = moment(formatHelper, "DD/MM/YYYY");
-
-        console.log(today);
-        console.log(expected);
-        console.log(formatHelper);
+        console.log(order.expected);
+        // const formatHelper = order.expected.match(/[0-9]{2}.[0-9]{2}.[0-9]{4}/)[0]
+        // const expected = moment(formatHelper, "DD/MM/YYYY");
         
-        const timeLeft = expected.diff(today, 'days');
+        // const timeLeft = expected.diff(today, 'days');
         
-        console.log('---------- timeLeft ----------');
-        console.log(timeLeft);
+        // if (timeLeft < 2 && timeLeft >= 0){
+        //     setWarning(true)
+        // }
 
-        if (timeLeft < 2 && timeLeft >= 0){
-            setWarning(true)
-        }
-
-        if (timeLeft < 0){
-            setIsLate(true)
-        }
+        // if (timeLeft < 0){
+        //     setIsLate(true)
+        // }
 
     },[order])
 
