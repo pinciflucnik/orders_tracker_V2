09668310d50e070d147fb108af03bc969cdd23f2
pinciflucnik <pinciflucnik1@gmail.com>
2025-04-17 18:18:19 +0300

    fix bug

--- src/components/order-list/list-item/ListItem.jsx
+++ src/components/order-list/list-item/ListItem.jsx
@@ -12,7 +12,7 @@ export default function ListItem({
     const {user} = useContext(AuthContext);
     const isOwner = useIsOwner(order.creator, user.username);
     const { isLate, warning } = useIsLate(order);
-    const formattedDate = moment(order.expected, ["DD.MM.YYYY", "MM.DD.YYYY", "DD.MM.YYYY г."], true).format("DD.MM.YYYY");
+    const formattedDate = moment(order.expected, ["DD.MM.YYYY", "MM.DD.YYYY", "DD.MM.YYYY г.", "MM/DD/YYYY", "DD/MM/YYYY", "DD/MM/YYYY г.", "M/DD/YYYY"], true).format("DD.MM.YYYY");
     console.log(order.expected);
 
 return (
